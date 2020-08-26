e464c926071bfebf48bde0ca1a222d05d4ab11b8
yujunkuo <johnboy880313@gmail.com>
2020-08-27 01:47:32 +0800

    Add FAQ Judge File & Complete the temporarily QA API

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -12,58 +12,56 @@ from transformers import BertForTokenClassification
 from transformers import BertTokenizer
 
 from .intent_classifier import IntentClassifier
+from .faq_judge import FAQJudge
 
 
 app = Flask(__name__)
 
 
-# BOOK_MODEL = BertForTokenClassification.from_pretrained("./book_model/")
-# FAQ_MODEL = SentenceTransformer("distiluse-base-multilingual-cased")
+BOOK_MODEL = BertForTokenClassification.from_pretrained("./book_model/")
+TOKENIZER = BertTokenizer.from_pretrained("bert-base-chinese")
 
-# Intent Classifier
-INTENT_CLASSIFIER = IntentClassifier()
+INTENT_CLASSIFIER = IntentClassifier()  # Intent Classifier
+FAQ_JUDGE = FAQ_JUDGE()  # FAQ Judge
 
-# ########### FAQ ############
-# df = pd.read_csv("./csv_file/faq_data.csv")
-# # faqs_dict is Question-Answer's pair
-# faqs_dict = dict()
-# for k, v in zip(df["問題"], df["答案"]):
-#     faqs_dict[k] = v
-# faqs = list(faqs_dict.keys())
-# faq_embeddings = sbert_model.encode(faqs)
+def get_answer(sentence: str):
+    faq_res = FAQ_JUDGE.get_faq_judge(sentence)
+    if faq_res: 
+        return faq_res
+    intent_res = INTENT_CLASSIFIER.get_intent_classification(sentence)
+    if intent_res == "search_book":
+        return _get_book_name(sentence)
+    elif intent_res == "borrow_place":
+        return "借場地嗎?"
+    else:
+        return "其他問題嗎?"
 
-# def calculate_cosine_similarity(x, y):
-#     # x = sentence_embeddings[0]
-#     # y = sentence_embeddings[1]
-#     res = np.dot(x, y) / (np.linalg.norm(x)*np.linalg.norm(y))
-#     return res
-#     # print("cosine similarity: {:.2f}".format(res))
+def _get_book_name(sentence: str):
+    book_tag_values = ["I-BOOK", "O", "B-BOOK", "PAD"]
+    tokenized_sentence = TOKENIZER.encode(sentence)
+    input_ids = torch.tensor([tokenized_sentence])
+    with torch.no_grad():
+        output = BOOK_MODEL(input_ids)
+    label_indices = np.argmax(output[0].to('cpu').numpy(), axis=2)
+    tokens = TOKENIZER.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
+    target, targets = [], []
+    for i, label_idx in enumerate(label_indices[0]):
+        label = book_tag_values[label_idx]
+        if label == "B-BOOK":
+            if target:
+                targets.append("".join(target))
+                target = []
+            target.append(tokens[i])
+        elif label == "I-BOOK":
+            target.append(tokens[i])
+    return targets[0]
 
-# def get_most_similar_faq_init(sentence):
-#     sentence_embedding = sbert_model.encode(sentence)[0]
-#     candidate, candidate_sim = None, -1
-#     for faq, faq_embedding in zip(faqs, faq_embeddings):
-#         sim = calculate_cosine_similarity(sentence_embedding, faq_embedding)
-#         if sim > candidate_sim:
-#             candidate_sim = sim
-#             candidate = faq
-#     # Cosine Similarity Threshold 0.85
-#     return (candidate, candidate_sim) if candidate_sim > 0.9 else None
-
-# print(get_most_similar_faq_init("如果我只有兩個人可以借討論室嗎"))
-# #################################
-
-
-# input_sentence = "請問有線性代數的課本嗎"
-# pred = get_intent_classification(intent_tokenizer, intent_model, label_map, input_sentence)
-# print("The preciction result: {}".format(pred))
-
-
+            
 @app.route("/", methods=["POST"])
-def get_answer():
+def home():
     data = request.get_json()
-    question = data["question"]
-    answer = INTENT_CLASSIFIER.get_intent_classification(question)
+    sentence = data["question"]
+    answer = get_answer(sentence)
     return_dict = {"answer": answer}
     return jsonify(return_dict)
 
---
+++ faq_judge.py
@@ -0,0 +1,37 @@
+## FAQ Judgement class
+
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+class FAQJudge:
+
+    def __init__(self):
+        self._model = SentenceTransformer("distiluse-base-multilingual-cased")
+        self._df = pd.read_csv("./csv_files/faq_data.csv")
+        # faqs_dict is Question-Answer's pair
+        self._faqs_dict = dict()
+        for k, v in zip(df["問題"], df["答案"]):
+            self._faqs_dict[k] = v
+        self._faqs = list(faqs_dict.keys())
+        self._faq_embeddings = self._model.encode(faqs)
+    
+    # (Public function) This is the API for FAQ Judgement
+    def get_faq_judge(self, sentence: str):
+        sentence_embedding = self._model.encode(sentence)[0]
+        candidate, candidate_sim = None, -1
+        for faq, faq_embedding in zip(self._faqs, self._faq_embeddings):
+            sim = self._cal_cosine_sim(sentence_embedding, faq_embedding)
+            if sim > candidate_sim:
+                candidate_sim, candidate = sim, faq
+        # Cosine Similarity Threshold 0.9
+        if (candidate is None) or (candidate_sim < 0.9):
+            return None
+        else:
+            answer = self._faqs_dict[candidate]
+            return answer
+
+    # (Protected function) Which is used to calculate cosine similarity
+    def _cal_cosine_sim(self, x, y):
+        res = np.dot(x, y) / (np.linalg.norm(x)*np.linalg.norm(y))
+        return res
