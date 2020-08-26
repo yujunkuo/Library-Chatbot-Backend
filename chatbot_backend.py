513583263d8c1a6e74591fbdafb8efd07afa1d1c
yujunkuo <johnboy880313@gmail.com>
2020-08-26 21:32:48 +0800

    Reconstruct the project configuration & Add Intent Classifier File

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,16 +1,70 @@
-from flask import Flask, request, jsonify
-from transformers import BertForSequenceClassification, BertForTokenClassification
+import pandas as pd
+import numpy as np
 import torch
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+from sentence_transformers import SentenceTransformer
+from transformers import BertForSequenceClassification
+from transformers import BertForTokenClassification
+from transformers import BertTokenizer
+
+from .intent_classifier import IntentClassifier
+
 
 app = Flask(__name__)
 
 
-INTENT_MODEL = BertForSequenceClassification.from_pretrained("./intent_model/")
-BOOK_MODEL = BertForTokenClassification.from_pretrained("./book_model/")
+# BOOK_MODEL = BertForTokenClassification.from_pretrained("./book_model/")
+# FAQ_MODEL = SentenceTransformer("distiluse-base-multilingual-cased")
+
+# Intent Classifier
+INTENT_CLASSIFIER = IntentClassifier()
+
+# ########### FAQ ############
+# df = pd.read_csv("./csv_file/faq_data.csv")
+# # faqs_dict is Question-Answer's pair
+# faqs_dict = dict()
+# for k, v in zip(df["問題"], df["答案"]):
+#     faqs_dict[k] = v
+# faqs = list(faqs_dict.keys())
+# faq_embeddings = sbert_model.encode(faqs)
+
+# def calculate_cosine_similarity(x, y):
+#     # x = sentence_embeddings[0]
+#     # y = sentence_embeddings[1]
+#     res = np.dot(x, y) / (np.linalg.norm(x)*np.linalg.norm(y))
+#     return res
+#     # print("cosine similarity: {:.2f}".format(res))
+
+# def get_most_similar_faq_init(sentence):
+#     sentence_embedding = sbert_model.encode(sentence)[0]
+#     candidate, candidate_sim = None, -1
+#     for faq, faq_embedding in zip(faqs, faq_embeddings):
+#         sim = calculate_cosine_similarity(sentence_embedding, faq_embedding)
+#         if sim > candidate_sim:
+#             candidate_sim = sim
+#             candidate = faq
+#     # Cosine Similarity Threshold 0.85
+#     return (candidate, candidate_sim) if candidate_sim > 0.9 else None
+
+# print(get_most_similar_faq_init("如果我只有兩個人可以借討論室嗎"))
+# #################################
+
+
+# input_sentence = "請問有線性代數的課本嗎"
+# pred = get_intent_classification(intent_tokenizer, intent_model, label_map, input_sentence)
+# print("The preciction result: {}".format(pred))
+
 
 @app.route("/", methods=["POST"])
-def home():
-    return_dict = {"response_text": "hello"}
+def get_answer():
+    data = request.get_json()
+    question = data["question"]
+    answer = INTENT_CLASSIFIER.get_intent_classification(question)
+    return_dict = {"answer": answer}
     return jsonify(return_dict)
 
 if __name__ == "__main__":
---
+++ intent_classifier.py
@@ -0,0 +1,67 @@
+## Intent classification class
+
+import torch
+
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification
+
+
+class IntentClassifier:
+
+    def __init__(self):
+        self._tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
+        self._model = BertForSequenceClassification.from_pretrained("./intent_model/")
+        self._label_map = {"search_book": 0, "borrow_place": 1, "other": 2}
+        self._index_map = {v: k for k, v in self._label_map.items()}
+
+    # (Public function) This is the API for Intent Classification
+    def get_intent_classification(self, sentence: str):
+        sentence_tensor = self._get_sentence_tensor(sentence)
+        pred = self._get_sentence_pred(sentence_tensor)
+        final_pred = self._index_map[int(pred[0])]
+        return final_pred
+
+    # (Protected function) Which is used to get sentence tensor
+    def _get_sentence_tensor(self, sentence: str):
+        # 建立句子的 BERT tokens
+        sentence_tokens = self._tokenizer.tokenize(sentence)
+        word_pieces = ["[CLS]"] + sentence_tokens + ["[SEP]"]
+        word_pieces_len = len(word_pieces)
+        # 將整個 token 序列轉換成索引序列
+        ids = self._tokenizer.convert_tokens_to_ids(word_pieces)
+        # Token Tensor
+        token_tensor = torch.tensor(ids)
+        # Segment Tensor
+        segment_tensor = torch.tensor([0] * word_pieces_len, dtype=torch.long)
+        # Mask Tensor
+        mask_tensor = torch.tensor([1] * word_pieces_len, dtype=torch.long)
+        # Label Tensor
+        label_tensor = None
+        return (token_tensor, segment_tensor, mask_tensor, label_tensor)
+
+    # (Protected function) Which is used to get sentence prediction
+    def _get_sentence_pred(self, sentence_tensor: tuple):
+        predictions = None
+        correct = 0
+        total = 0
+        
+        with torch.no_grad():
+
+            # 將所有 tensors 移到 GPU 上
+            if next(self._model.parameters()).is_cuda:
+                sentence_tensor = [t.to("cuda:0") for t in sentence_tensor if t is not None]
+            
+            # 前 3 個 tensors 分別為 token, segment 以及 mask
+            # 建議在將這些 tensors 丟入 model 時指定對應的參數名稱
+            token_tensor, segment_tensor, mask_tensor = sentence_tensor[:3]
+            token_tensor = token_tensor.resize_((1, token_tensor.shape[0]))
+            segment_tensor = segment_tensor.resize_((1, segment_tensor.shape[0]))
+            mask_tensor = mask_tensor.resize_((1, mask_tensor.shape[0]))
+            outputs = self._model(input_ids=token_tensor, 
+                            token_type_ids=segment_tensor, 
+                            attention_mask=mask_tensor)
+            
+            logits = outputs[0]
+            _, pred = torch.max(logits.data, 1)
+
+        return pred
