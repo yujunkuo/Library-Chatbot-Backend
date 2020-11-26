4538ef0b87493ae8c2150dc0489fcced6120a5e3
yujunkuo <johnboy880313@gmail.com>
2020-11-26 17:02:44 +0800

    Facility similarity function modified

--- faci_intent.py
+++ faci_intent.py
@@ -10,11 +10,12 @@ import pandas as pd
 
 from transformers import BertTokenizer
 from transformers import BertForTokenClassification
+from sentence_transformers import SentenceTransformer
 
 
 FACI_MODEL = BertForTokenClassification.from_pretrained("./models/faci_model/")
+SENTENCE_MODEL = SentenceTransformer("distiluse-base-multilingual-cased")
 TOKENIZER = BertTokenizer.from_pretrained("bert-base-chinese")
-
 
 # Public API to get facility information
 def get_faci_info(sentence: str):
@@ -22,7 +23,6 @@ def get_faci_info(sentence: str):
     faci_info = _get_faci_info(facis)
     faci_info["class"] = "facility"
     return faci_info
-
 
 # (Protected function) Get facility name
 def _get_faci_name(sentence: str):
@@ -53,6 +53,26 @@ def _get_faci_info(facis):
     faci = facis[0] if facis else "無此場地"
     df = pd.read_csv("./csv_files/faci.csv")
     for idx, val in df.iterrows():
+        # Judge whether this entity is in the entities list
         if (faci in val.entity) or (val.entity in faci):
             return {"faci_name": val["entity"], "introduce": val["introduce"], "floor": val["floor"], "number": val["number"], "classify": val["classify(1:設備,2:服務,3:場地,4:館藏資料)"]}
-    return {"faci_name": "", "introduce": "", "floor": "", "number": "", "classify": ""}
+    entity_list_embedding = SENTENCE_MODEL.encode(list(df["entity"]))
+    current_embedding = SENTENCE_MODEL.encode(faci)
+    candidate, candidate_sim = None, -1
+    for entity, entity_embedding in zip(list(df["entity"]), entity_list_embedding):
+        sim = _cal_cosine_sim(current_embedding, entity_embedding)
+        if sim > candidate_sim:
+            candidate_sim, candidate = sim, entity
+    # Cosine Similarity Threshold 0.8
+    if (candidate is None) or (candidate_sim < 0.8):
+        print("設備相似度: ", candidate_sim)
+        return {"faci_name": "", "introduce": "", "floor": "", "number": "", "classify": ""}
+    else:
+        print("設備相似度: ", candidate_sim)
+        val = df[df["entity"] == candidate]
+        return {"faci_name": val["entity"], "introduce": val["introduce"], "floor": val["floor"], "number": val["number"], "classify": val["classify(1:設備,2:服務,3:場地,4:館藏資料)"]}
+
+# (Protected function) Calculate Cosine Similarity between to sentences
+def _cal_cosine_sim(self, x, y):
+    res = np.dot(x, y) / (np.linalg.norm(x)*np.linalg.norm(y))
+    return res
